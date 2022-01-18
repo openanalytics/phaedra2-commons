0da4bb31efe33676a65a458785067a459170bd2d
Frederick Michielssen <fmichielssen@openanalytics.eu>
2022-01-18 15:00:05 +0100

    Added debug logging to auth helper

--- src/main/java/eu/openanalytics/phaedra/util/auth/AuthorizationHelper.java
+++ src/main/java/eu/openanalytics/phaedra/util/auth/AuthorizationHelper.java
@@ -3,6 +3,8 @@ package eu.openanalytics.phaedra.util.auth;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.jwt.Jwt;
 
 public class AuthorizationHelper {
@@ -14,6 +16,8 @@ public class AuthorizationHelper {
 	
 	private static final String ROLE_TEAM_PREFIX = "phaedra2-team-";
 
+	private static final Logger log = LoggerFactory.getLogger(AuthorizationHelper.class);
+	
 	public static boolean hasUserAccess(Jwt accessToken) {
 		return hasRole(accessToken, ROLE_USER);
 	}
@@ -30,6 +34,8 @@ public class AuthorizationHelper {
 		if (accessToken == null) return false;
 		
 		List<String> roles = accessToken.getClaimAsStringList(CLAIM_ROLES);
+		log.debug(String.format("Inspecting JWT token for role '%s'. Available roles: %s", roleName, roles));
+		
 		if (roles == null || roles.isEmpty()) return false;
 		
 		return roles.stream().anyMatch(role -> role.equalsIgnoreCase(roleName));
